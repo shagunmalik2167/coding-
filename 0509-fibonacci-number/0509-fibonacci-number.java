4e1740c677fb59f4c66433001efe4c293bcbc13a
Shagun Malik <shagunmalik2167@gmail.com>
2026-09-15 23:49:16 +0530

    Time: 0 ms (100%), Space: 41.9 MB (70.25%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fibo(int n ,int[]dp) {
+        if(n<= 1) return n;
+        if(dp[n] != 0) return dp[n];
+        return dp[n] = fibo(n-1, dp) + fibo(n-2, dp);
+    }
+        public int fib(int n){
+            int [] dp = new int [n+1];
+        
+         return fibo(n, dp);
+  
+    }
+}
